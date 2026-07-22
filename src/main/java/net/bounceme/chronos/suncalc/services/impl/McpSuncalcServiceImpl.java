c7f711a677e0111790b6b21b290a8bae5c996295
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-07-22 12:33:36 +0200

    Remove support

--- src/main/java/net/bounceme/chronos/suncalc/services/impl/McpSuncalcServiceImpl.java
+++ src/main/java/net/bounceme/chronos/suncalc/services/impl/McpSuncalcServiceImpl.java
@@ -1,14 +1,9 @@
 package net.bounceme.chronos.suncalc.services.impl;
 
-import java.util.Map;
-
 import org.springaicommunity.mcp.annotation.McpTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import io.modelcontextprotocol.server.McpSyncServerExchange;
-import io.modelcontextprotocol.spec.McpSchema.LoggingLevel;
-import io.modelcontextprotocol.spec.McpSchema.LoggingMessageNotification;
 import net.bounceme.chronos.suncalc.model.TimeData;
 import net.bounceme.chronos.suncalc.services.SuncalcService;
 
@@ -16,19 +11,10 @@ import net.bounceme.chronos.suncalc.services.SuncalcService;
 public class McpSuncalcServiceImpl {
 	
 	@Autowired
-	private McpSyncServerExchange exchange;
-	
-	@Autowired
 	private SuncalcService suncalcService;
 
 	@McpTool(description = "get dawn, sunrise, culmination, sunset and dusk for the current time")
 	public TimeData getCurrentTimeData() {
-		exchange.loggingNotification(LoggingMessageNotification.builder() // (3)
-				.level(LoggingLevel.DEBUG)
-				.data("Call getCurrentTimeData Tool with current time")
-				.meta(Map.of()) // non null meta as a workaround for bug: ...
-				.build());
-		
 		return suncalcService.getCurrentTimeData();
 	}
 }
