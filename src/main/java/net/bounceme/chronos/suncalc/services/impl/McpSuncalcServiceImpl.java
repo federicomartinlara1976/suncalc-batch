92aebc4f76de31804967b17df5771eb48834aab5
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-07-23 10:58:02 +0200

    Add logging capability

--- src/main/java/net/bounceme/chronos/suncalc/services/impl/McpSuncalcServiceImpl.java
+++ src/main/java/net/bounceme/chronos/suncalc/services/impl/McpSuncalcServiceImpl.java
@@ -1,9 +1,14 @@
 package net.bounceme.chronos.suncalc.services.impl;
 
+import java.util.Map;
+
 import org.springaicommunity.mcp.annotation.McpTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.modelcontextprotocol.server.McpSyncServerExchange;
+import io.modelcontextprotocol.spec.McpSchema.LoggingLevel;
+import io.modelcontextprotocol.spec.McpSchema.LoggingMessageNotification;
 import net.bounceme.chronos.suncalc.model.TimeData;
 import net.bounceme.chronos.suncalc.services.SuncalcService;
 
@@ -14,7 +19,13 @@ public class McpSuncalcServiceImpl {
 	private SuncalcService suncalcService;
 
 	@McpTool(description = "get dawn, sunrise, culmination, sunset and dusk for the current time")
-	public TimeData getCurrentTimeData() {
+	public TimeData getCurrentTimeData(McpSyncServerExchange exchange) {
+		exchange.loggingNotification(LoggingMessageNotification.builder()
+				.level(LoggingLevel.DEBUG)
+				.data("Call getCurrentTimeData")
+				.meta(Map.of()) // non null meata as a workaround for bug: ...
+				.build());
+		
 		return suncalcService.getCurrentTimeData();
 	}
 }
