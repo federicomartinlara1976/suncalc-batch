682c0f76126d421e6c372fcbdc132bd6556772e6
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-14 14:08:40 +0200

    Nueva función para calcular los días de un mes

--- src/main/java/net/bounceme/chronos/suncalc/support/SuncalcHelper.java
+++ src/main/java/net/bounceme/chronos/suncalc/support/SuncalcHelper.java
@@ -108,4 +108,22 @@ public class SuncalcHelper {
 		
 		return 0L;
 	}
+	
+	public static int getDiasDelMes(Integer month, Integer year) {
+	    switch (month) {
+	        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+	            return 31;
+	        case 4: case 6: case 9: case 11:
+	            return 30;
+	        case 2:
+	            return esBisiesto(year) ? 29 : 28;
+	        default:
+	            throw new IllegalArgumentException("Mes inválido: " + month);
+	    }
+	}
+
+	private static boolean esBisiesto(Integer year) {
+	    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	}
+
 }
