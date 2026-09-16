e8b245e7a1c3c5447de8a5b7ec7f811005d1c785
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-16 08:47:42 +0200

    Refactor

--- src/main/java/net/bounceme/chronos/suncalc/scheduler/PeriodicTasks.java
+++ src/main/java/net/bounceme/chronos/suncalc/scheduler/PeriodicTasks.java
@@ -1,17 +1,15 @@
 package net.bounceme.chronos.suncalc.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 import net.bounceme.chronos.suncalc.dto.JobDTO;
 import net.bounceme.chronos.suncalc.dto.TaskDTO;
 import net.bounceme.chronos.suncalc.facade.JobFacade;
 
-@Configuration
-@Slf4j
+@Component
 @EnableScheduling
 public class PeriodicTasks {
 	
@@ -19,7 +17,7 @@ public class PeriodicTasks {
 	private JobFacade jobFacade;
 	
 	@Scheduled(cron = "${application.importTimes.cron}")
-    public void importJobTask() {
+    public void importTimesTask() {
 		JobDTO<TaskDTO> jobDTO = new JobDTO<>();
 		jobDTO.setContent(TaskDTO.builder().name("importTimes").build());
 		
