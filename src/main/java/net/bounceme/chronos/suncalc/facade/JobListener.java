e99c1ebb66ea5cea07dff57991c1cf12d6f697bc
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-16 08:11:01 +0200

    Convert to map

--- src/main/java/net/bounceme/chronos/suncalc/facade/JobListener.java
+++ src/main/java/net/bounceme/chronos/suncalc/facade/JobListener.java
@@ -8,31 +8,29 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import net.bounceme.chronos.suncalc.dto.JobDTO;
-import net.bounceme.chronos.suncalc.dto.MonthYearDTO;
 import net.bounceme.chronos.suncalc.model.ExecutionResult;
-import net.bounceme.chronos.suncalc.model.Task;
 import net.bounceme.chronos.suncalc.services.JobService;
 
 @Component
 @Slf4j
 public class JobListener {
-	
+
 	@Autowired
 	private JobService jobService;
-	
+
+	@SuppressWarnings("unchecked")
 	@RabbitListener(queues = "SuncalcEventos")
 	public void executeJob(JobDTO<?> jobDTO) {
 		Map<String, Object> content = (Map<String, Object>) jobDTO.getContent();
-		
-		// TODO - Esto cambia por entrada de mapa
-		if (content instanceof Task) {
-			ExecutionResult resultado = jobService.run(((Task) content).getName());
+
+		if (content.containsKey("name")) {
+			ExecutionResult resultado = jobService.run((String) content.get("name"));
 			log.info("{}", resultado);
 		}
-		
-		if (content instanceof MonthYearDTO) {
-			MonthYearDTO monthYearDTO = (MonthYearDTO) content;
-			ExecutionResult resultado = jobService.runImportByMonthAndYear(monthYearDTO.getYear(), monthYearDTO.getMonth());
+
+		if (content.containsKey("year")) {
+			ExecutionResult resultado = jobService.runImportByMonthAndYear((Integer) content.get("year"),
+					(Integer) content.get("month"));
 			log.info("{}", resultado);
 		}
 	}
