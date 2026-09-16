2915a69173abeb7213f808a12f4cfe8e554ec8bd
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-16 13:06:08 +0200

    Añadir cláusula de ordenamiento

--- src/main/java/net/bounceme/chronos/suncalc/repository/TimeDataRepository.java
+++ src/main/java/net/bounceme/chronos/suncalc/repository/TimeDataRepository.java
@@ -2,6 +2,7 @@ package net.bounceme.chronos.suncalc.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -10,5 +11,5 @@ import net.bounceme.chronos.suncalc.model.TimeData;
 public interface TimeDataRepository extends MongoRepository<TimeData, String> {
 
 	@Query("{ $and : [ { '_id' : { $gte : ?0 } }, { '_id' : { $lte : ?1 } } ] }")
-	List<TimeData> listRegistros(String from, String to);
+	List<TimeData> listRegistros(String from, String to, Sort sort);
 }
