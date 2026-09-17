b8469818187cecd93a857bf6e6a9b5736016030a
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-17 12:39:36 +0200

    Remove unused param

--- src/main/java/net/bounceme/chronos/suncalc/controller/JobController.java
+++ src/main/java/net/bounceme/chronos/suncalc/controller/JobController.java
@@ -133,7 +133,7 @@ public class JobController {
 	}
 
 	@GetMapping("/scheduling")
-	public ResponseEntity<Map<String, Object>> schedulingJob(@Valid @RequestParam String name, BindingResult result) {
+	public ResponseEntity<Map<String, Object>> schedulingJob(@Valid @RequestParam String name) {
 		Map<String, Object> response = new HashMap<>();
 
 		String scheduling = jobService.getJobScheduling(name);
