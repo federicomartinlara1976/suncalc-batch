f922826fc4ed9aef0dd059c7c912a51db532de66
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-09-25 14:31:27 +0200

    Cálculo para un año en concreto

--- src/main/java/net/bounceme/chronos/suncalc/controller/AstronomiaController.java
+++ src/main/java/net/bounceme/chronos/suncalc/controller/AstronomiaController.java
@@ -1,5 +1,7 @@
 package net.bounceme.chronos.suncalc.controller;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +30,21 @@ public class AstronomiaController {
 	public ResponseEntity<Map<String, Object>> getCurrent() {
 		Map<String, Object> response = new HashMap<>();
 
-		List<DataSolsticeEquinoxDTO> results = astronomiaService.calculateSolsticesEquinoxes(2026);
+		Date current = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(current);
+		List<DataSolsticeEquinoxDTO> results = astronomiaService.calculateSolsticesEquinoxes(cal.get(Calendar.YEAR));
+		
+		response.put("result", results);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+	
+	@GetMapping("year/{year}")
+	public ResponseEntity<Map<String, Object>> getForYear(@PathVariable Integer year) {
+		Map<String, Object> response = new HashMap<>();
+
+		List<DataSolsticeEquinoxDTO> results = astronomiaService.calculateSolsticesEquinoxes(year);
+		
 		response.put("result", results);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
